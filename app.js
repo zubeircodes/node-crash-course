f96bb6e76143df27fe804e316dd8dcde4bc187b9
Zubeir <zubeir06@yahoo.com>
2023-06-11 21:06:01 -0500

    configured mongoose

--- app.js
+++ app.js
@@ -1,22 +1,63 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
 // connect to mongodb
-const dbURI = 'mongodb+srv://zubeiro:zube12345@nodetuts.x8jrq25.mongodb.net/'
-
+const dbURI = 'mongodb+srv://zubeiro:zube12345@nodetuts.x8jrq25.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then((result) => {
+  app.listen(3000);
+}) 
+.catch((err) => console.log(err));
 // register view engine 
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
 
 //middleware & static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
+
+// mongoose and mongo sandbo routes
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog 2',
+    snippet: 'about my new blog',
+    body: 'more about my new blog'
+  });
+
+  blog.save()
+  .then((result) => {
+    res.send(result)
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+})
+
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+  .then((result) => {
+    res.send(result)
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+});
+
+app.get('/single-blog', (req, res) => {
+  Blog.findById('64865e5d38618fee6681e1a2')
+  .then((result) => {
+    res.send(result)
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+});
 
 app.get('/', (req, res) => {
   const blogs = [
